f0c54a2274d58f1fe01d6903d616b8a55cfc1b9f
simoneya <yavar.simona@yahoo.com>
2023-01-14 14:28:39 +0000

    setting app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+
 function App() {
+
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-      
+      <h1> Hello There!</h1>
+      <input />
+      <button>Send Message</button>
+      {/* input field */}
+      {/* button */}
+
+      {/* messages themselves */}
     </div>
   );
 }
