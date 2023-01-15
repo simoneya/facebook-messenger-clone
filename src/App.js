7a80474788e6074fa723df594de117a6edbd38f0
simoneya <yavar.simona@yahoo.com>
2023-01-15 10:36:21 +0000

    update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import './App.css';
 function App() {
 
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+    console.log(input);
+    console.log(messages);
+
+  const sendMessage = (event) => {
+      //all the logic to send a message goes here.
+      setMessages([...messages, input]);
+      setInput("");
+  }
 
   return (
     <div className="App">
       <h1> Hello There!</h1>
-      <input />
-      <button>Send Message</button>
-      {/* input field */}
-      {/* button */}
-
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={sendMessage}>Send Message</button>
+      
       {/* messages themselves */}
+
+      {
+        messages.map(message => (
+          <p>{messages}</p>
+        ))
+      }
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDf_t3xTC2bTJpWwIH0Bbwf8SILa0HfEGY",
+    authDomain: "facebook-messenger-clone-a5374.firebaseapp.com",
+    projectId: "facebook-messenger-clone-a5374",
+    storageBucket: "facebook-messenger-clone-a5374.appspot.com",
+    messagingSenderId: "770570755340",
+    appId: "1:770570755340:web:78d10757d873d1aefebd29",
+    measurementId: "G-56PDBL6SL4"
+  };
